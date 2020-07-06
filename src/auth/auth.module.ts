32963271f01622ba06d1cf1b998278822f8729b7
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2020-07-05 23:15:46 -0300

    setting up the jwt strategy authorization and added custom @GetUser Decorator

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -19,6 +20,13 @@ import { PassportModule } from '@nestjs/passport';
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    JwtStrategy,
+  ],
+  exports: [
+    JwtStrategy,
+    PassportModule,
+  ]
 })
 export class AuthModule {}
