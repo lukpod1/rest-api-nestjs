11e54af381687476f90cb40f2963ce70284d4015
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2020-07-06 23:52:13 -0300

    testing TasksService: createTask

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -10,6 +10,7 @@ const mockUser = { id: 12, username: 'Test user' };
 const mockTaskRepository = () => ({
     getTasks: jest.fn(),
     findOne: jest.fn(),
+    createTask: jest.fn(),
 })
 
 describe('TasksService', () => {
@@ -33,10 +34,10 @@ describe('TasksService', () => {
         it('gets all tasks from the repository', async () => {
             taskRepository.getTasks.mockResolvedValue('someValue');
             expect(taskRepository.getTasks).not.toHaveBeenCalled();
-            
-            const filters: TaskFilterDto = { status: Status.IN_PROGRESS, search: 'Some search query' };    
+
+            const filters: TaskFilterDto = { status: Status.IN_PROGRESS, search: 'Some search query' };
             const result = await tasksService.getTasks(filters, mockUser);
-            
+
             expect(taskRepository.getTasks).toHaveBeenCalled();
             expect(result).toEqual('someValue');
         })
@@ -44,24 +45,37 @@ describe('TasksService', () => {
 
     describe('getTaskById', () => {
         it('calls taskRepository.findOne() and succesffuly retrieve and return the task', async () => {
-          const mockTask = { title: 'Test task', description: 'Test desc' };
-          taskRepository.findOne.mockResolvedValue(mockTask);
-    
-          const result = await tasksService.getTaskById(1, mockUser);
-          expect(result).toEqual(mockTask);
-    
-          expect(taskRepository.findOne).toHaveBeenCalledWith({
-            where: {
-              id: 1,
-              userId: mockUser.id,
-            },
-          });
+            const mockTask = { title: 'Test task', description: 'Test desc' };
+            taskRepository.findOne.mockResolvedValue(mockTask);
+
+            const result = await tasksService.getTaskById(1, mockUser);
+            expect(result).toEqual(mockTask);
+
+            expect(taskRepository.findOne).toHaveBeenCalledWith({
+                where: {
+                    id: 1,
+                    userId: mockUser.id,
+                },
+            });
         });
-    
+
         it('throws an error as task is not found', () => {
-          taskRepository.findOne.mockResolvedValue(null);
-          expect(tasksService.getTaskById(1, mockUser)).rejects.toThrow(NotFoundException);
+            taskRepository.findOne.mockResolvedValue(null);
+            expect(tasksService.getTaskById(1, mockUser)).rejects.toThrow(NotFoundException);
         });
-      });
+    });
+
+    describe('createTask', () => {
+        it('calls taskRepository.create() and returns the result', async () => {
+            taskRepository.createTask.mockResolvedValue('someTask');
+            expect(taskRepository.createTask).not.toHaveBeenCalled();
+
+            const createTaskDto = { title: 'Test task', description: 'Test desc' };
+            const result = await tasksService.createTask(createTaskDto, mockUser);
+            
+            expect(taskRepository.createTask).toHaveBeenCalledWith(createTaskDto, mockUser);
+            expect(result).toEqual('someTask');
+        });
+    });
 
 })
